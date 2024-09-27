1cd744c6370d246c216f972629bc9c720b078be9
Dominykas Stunžėnas <dominykas.stunzenas@stud.vilniustech.lt>
2024-09-27 14:06:09 +0300

    repositories for data manipulation in database

--- app/src/main/java/book/exchange/app/repository/ChatRepository.java
+++ app/src/main/java/book/exchange/app/repository/ChatRepository.java
@@ -1,2 +1,59 @@
-package book.exchange.app.repository;public interface ChatRepository {
+package book.exchange.app.repository;
+
+import book.exchange.app.model.Chat;
+import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+@Mapper
+public interface ChatRepository {
+
+    @Insert("INSERT INTO app.chats " +
+            "(id, user_id, book_id, comic_id, periodical_id) " +
+            "VALUES(#{id}, #{userId}, #{bookId}, #{comicId}, #{periodicalId})")
+    void createChat(Chat chat);
+
+    @Select("SELECT * FROM app.chats WHERE user_id = #{userId}")
+    @Results({
+            @Result(property = "id", column = "id"),
+            @Result(property = "userId", column = "user_id"),
+            @Result(property = "bookId", column = "book_id"),
+            @Result(property = "comicId", column = "comic_id"),
+            @Result(property = "periodicalId", column = "periodical_id")
+    })
+    List<Chat> getChatsByUser(@Param("userId") UUID id);
+
+    @Select("SELECT * FROM app.chats WHERE user_id = #{userId} AND book_id = #{bookId}")
+    @Results({
+            @Result(property = "id", column = "id"),
+            @Result(property = "userId", column = "user_id"),
+            @Result(property = "bookId", column = "book_id"),
+            @Result(property = "comicId", column = "comic_id"),
+            @Result(property = "periodicalId", column = "periodical_id")
+    })
+    Optional<Chat> getChatForBook(@Param("userId") UUID userId, @Param("bookId") UUID bookId);
+
+    @Select("SELECT * FROM app.chats WHERE user_id = #{userId} AND comic_id = #{comicId}")
+    @Results({
+            @Result(property = "id", column = "id"),
+            @Result(property = "userId", column = "user_id"),
+            @Result(property = "bookId", column = "book_id"),
+            @Result(property = "comicId", column = "comic_id"),
+            @Result(property = "periodicalId", column = "periodical_id")
+    })
+    Optional<Chat> getChatForComic(@Param("userId") UUID userId, @Param("comicId") UUID comicId);
+
+    @Select("SELECT * FROM app.chats WHERE user_id = #{userId} AND periodical_id = #{periodicalId}")
+    @Results({
+            @Result(property = "id", column = "id"),
+            @Result(property = "userId", column = "user_id"),
+            @Result(property = "bookId", column = "book_id"),
+            @Result(property = "comicId", column = "comic_id"),
+            @Result(property = "periodicalId", column = "periodical_id")
+    })
+    Optional<Chat> getChatForPeriodical(@Param("userId") UUID userID, @Param("periodicalId") UUID periodicalId);
 }
--- app/src/main/java/book/exchange/app/repository/PeriodicalRepository.java
+++ app/src/main/java/book/exchange/app/repository/PeriodicalRepository.java
@@ -1,2 +1,163 @@
-package book.exchange.app.repository;public interface PeriodicalRepository {
+package book.exchange.app.repository;
+
+import book.exchange.app.model.Periodical;
+import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+@Mapper
+public interface PeriodicalRepository {
+
+    @Insert("INSERT INTO app.periodicals " +
+            "(id, user_id, time_posted, release_year, title, publisher, author, book_language, status, price, number) " +
+            "VALUES(#{id}, #{userId}, #{timePosted}, #{releaseYear}, #{title}, #{publisher}, #{author}, #{bookLanguage}, #{status}, #{price}, #{number})")
+    void createPeriodical(Periodical periodical);
+
+    @Select("SELECT * FROM app.periodicals")
+    @Results({
+            @Result(property = "id", column = "id"),
+            @Result(property = "userId", column = "user_id"),
+            @Result(property = "timePosted", column = "time_posted"),
+            @Result(property = "releaseYear", column = "release_year"),
+            @Result(property = "title", column = "title"),
+            @Result(property = "publisher", column = "publisher"),
+            @Result(property = "author", column = "author"),
+            @Result(property = "bookLanguage", column = "book_language"),
+            @Result(property = "status", column = "status"),
+            @Result(property = "price", column = "price"),
+            @Result(property = "number", column = "number")
+    })
+    List<Periodical> findAll();
+
+    @Select("SELECT * FROM app.periodicals WHERE id = #{id}")
+    @Results({
+            @Result(property = "id", column = "id"),
+            @Result(property = "userId", column = "user_id"),
+            @Result(property = "timePosted", column = "time_posted"),
+            @Result(property = "releaseYear", column = "release_year"),
+            @Result(property = "title", column = "title"),
+            @Result(property = "publisher", column = "publisher"),
+            @Result(property = "author", column = "author"),
+            @Result(property = "bookLanguage", column = "book_language"),
+            @Result(property = "status", column = "status"),
+            @Result(property = "price", column = "price"),
+            @Result(property = "number", column = "number")
+    })
+    Optional<Periodical> findById(@Param("id") UUID id);
+
+    @Select("SELECT * FROM app.periodicals WHERE release_year = #{releaseYear}")
+    @Results({
+            @Result(property = "id", column = "id"),
+            @Result(property = "userId", column = "user_id"),
+            @Result(property = "timePosted", column = "time_posted"),
+            @Result(property = "releaseYear", column = "release_year"),
+            @Result(property = "title", column = "title"),
+            @Result(property = "publisher", column = "publisher"),
+            @Result(property = "author", column = "author"),
+            @Result(property = "bookLanguage", column = "book_language"),
+            @Result(property = "status", column = "status"),
+            @Result(property = "price", column = "price"),
+            @Result(property = "number", column = "number")
+    })
+    List<Periodical> findByYear(@Param("releaseYear") Integer releaseYear);
+
+    @Select("SELECT * FROM app.periodicals WHERE price <= #{price}")
+    @Results({
+            @Result(property = "id", column = "id"),
+            @Result(property = "userId", column = "user_id"),
+            @Result(property = "timePosted", column = "time_posted"),
+            @Result(property = "releaseYear", column = "release_year"),
+            @Result(property = "title", column = "title"),
+            @Result(property = "publisher", column = "publisher"),
+            @Result(property = "author", column = "author"),
+            @Result(property = "bookLanguage", column = "book_language"),
+            @Result(property = "status", column = "status"),
+            @Result(property = "price", column = "price"),
+            @Result(property = "number", column = "number")
+    })
+    List<Periodical> findByPrice(@Param("price") Double price);
+
+    @Select("SELECT * FROM app.periodicals WHERE LOWER(title) ILIKE '%' || LOWER(#{title}) || '%'")
+    @Results({
+            @Result(property = "id", column = "id"),
+            @Result(property = "userId", column = "user_id"),
+            @Result(property = "timePosted", column = "time_posted"),
+            @Result(property = "releaseYear", column = "release_year"),
+            @Result(property = "title", column = "title"),
+            @Result(property = "publisher", column = "publisher"),
+            @Result(property = "author", column = "author"),
+            @Result(property = "bookLanguage", column = "book_language"),
+            @Result(property = "status", column = "status"),
+            @Result(property = "price", column = "price"),
+            @Result(property = "number", column = "number")
+    })
+    List<Periodical> findByTitle(@Param("title") String title);
+
+    @Select("SELECT * FROM app.periodicals WHERE LOWER(publisher) ILIKE '%' || LOWER(#{publisher}) || '%'")
+    @Results({
+            @Result(property = "id", column = "id"),
+            @Result(property = "userId", column = "user_id"),
+            @Result(property = "timePosted", column = "time_posted"),
+            @Result(property = "releaseYear", column = "release_year"),
+            @Result(property = "title", column = "title"),
+            @Result(property = "publisher", column = "publisher"),
+            @Result(property = "author", column = "author"),
+            @Result(property = "bookLanguage", column = "book_language"),
+            @Result(property = "status", column = "status"),
+            @Result(property = "price", column = "price"),
+            @Result(property = "number", column = "number")
+    })
+    List<Periodical> findByPublisher(@Param("publisher") String publisher);
+
+    @Select("SELECT * FROM app.periodicals WHERE LOWER(author) ILIKE '%' || LOWER(#{author}) || '%'")
+    @Results({
+            @Result(property = "id", column = "id"),
+            @Result(property = "userId", column = "user_id"),
+            @Result(property = "timePosted", column = "time_posted"),
+            @Result(property = "releaseYear", column = "release_year"),
+            @Result(property = "title", column = "title"),
+            @Result(property = "publisher", column = "publisher"),
+            @Result(property = "author", column = "author"),
+            @Result(property = "bookLanguage", column = "book_language"),
+            @Result(property = "status", column = "status"),
+            @Result(property = "price", column = "price"),
+            @Result(property = "number", column = "number")
+    })
+    List<Periodical> findByAuthor(@Param("author") String author);
+
+    @Select("SELECT * FROM app.periodicals WHERE LOWER(book_language) ILIKE '%' || LOWER(#{bookLanguage}) || '%'")
+    @Results({
+            @Result(property = "id", column = "id"),
+            @Result(property = "userId", column = "user_id"),
+            @Result(property = "timePosted", column = "time_posted"),
+            @Result(property = "releaseYear", column = "release_year"),
+            @Result(property = "title", column = "title"),
+            @Result(property = "publisher", column = "publisher"),
+            @Result(property = "author", column = "author"),
+            @Result(property = "bookLanguage", column = "book_language"),
+            @Result(property = "status", column = "status"),
+            @Result(property = "price", column = "price"),
+            @Result(property = "number", column = "number")
+    })
+    List<Periodical> findByBookLanguage(@Param("bookLanguage") String bookLanguage);
+
+    @Select("SELECT * FROM app.periodicals WHERE LOWER(status) ILIKE '%' || LOWER(#{status}) || '%'")
+    @Results({
+            @Result(property = "id", column = "id"),
+            @Result(property = "userId", column = "user_id"),
+            @Result(property = "timePosted", column = "time_posted"),
+            @Result(property = "releaseYear", column = "release_year"),
+            @Result(property = "title", column = "title"),
+            @Result(property = "publisher", column = "publisher"),
+            @Result(property = "author", column = "author"),
+            @Result(property = "bookLanguage", column = "book_language"),
+            @Result(property = "status", column = "status"),
+            @Result(property = "price", column = "price"),
+            @Result(property = "number", column = "number")
+    })
+    List<Periodical> findByStatus(@Param("status") String status);
 }
