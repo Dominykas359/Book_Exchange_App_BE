09b9931e9793b23255432d23c798a668e6c0d760
Dominykas Stunžėnas <dominykas.stunzenas@stud.vilniustech.lt>
2024-09-28 15:06:25 +0300

    created mappers

--- app/src/main/java/book/exchange/app/mapper/ComicMapper.java
+++ app/src/main/java/book/exchange/app/mapper/ComicMapper.java
@@ -1,2 +1,42 @@
-package book.exchange.app.mapper;public class ComicMapper {
+package book.exchange.app.mapper;
+
+import book.exchange.app.dto.comicDTOs.ComicRequestDTO;
+import book.exchange.app.dto.comicDTOs.ComicResponseDTO;
+import book.exchange.app.model.Comic;
+
+import java.util.UUID;
+
+public class ComicMapper {
+
+    public static ComicResponseDTO toDto(Comic comic){
+
+        return ComicResponseDTO.builder()
+                .id(comic.getId())
+                .releaseYear(comic.getReleaseYear())
+                .title(comic.getTitle())
+                .publisher(comic.getPublisher())
+                .author(comic.getAuthor())
+                .language(comic.getLanguage())
+                .status(comic.getStatus())
+                .price(comic.getPrice())
+                .pageCount(comic.getPageCount())
+                .colored(comic.getColored())
+                .build();
+    }
+
+    public static Comic fromDto(ComicRequestDTO comicRequestDTO){
+
+        return Comic.builder()
+                .id(UUID.randomUUID())
+                .releaseYear(comicRequestDTO.getReleaseYear())
+                .title(comicRequestDTO.getTitle())
+                .publisher(comicRequestDTO.getPublisher())
+                .author(comicRequestDTO.getAuthor())
+                .language(comicRequestDTO.getLanguage())
+                .status(comicRequestDTO.getStatus())
+                .price(comicRequestDTO.getPrice())
+                .pageCount(comicRequestDTO.getPageCount())
+                .colored(comicRequestDTO.getColored())
+                .build();
+    }
 }
--- app/src/main/java/book/exchange/app/mapper/CommentMapper.java
+++ app/src/main/java/book/exchange/app/mapper/CommentMapper.java
@@ -1,2 +1,35 @@
-package book.exchange.app.mapper;public class CommentMapper {
+package book.exchange.app.mapper;
+
+import book.exchange.app.dto.commentDTOs.CommentRequestDTO;
+import book.exchange.app.dto.commentDTOs.CommentResponseDTO;
+import book.exchange.app.model.Comment;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class CommentMapper {
+
+    public static CommentResponseDTO toDto(Comment comment){
+
+        return CommentResponseDTO.builder()
+                .id(comment.getId())
+                .userId(comment.getUserId())
+                .timeSent(comment.getTimeSent())
+                .content(comment.getContent())
+                .commentId(comment.getCommentId())
+                .noticeId(comment.getNoticeId())
+                .build();
+    }
+
+    public static Comment fromDto(CommentRequestDTO commentRequestDTO){
+
+        return Comment.builder()
+                .id(UUID.randomUUID())
+                .userId(commentRequestDTO.getUserId())
+                .timeSent(LocalDateTime.now())
+                .content(commentRequestDTO.getContent())
+                .commentId(commentRequestDTO.getCommentId())
+                .noticeId(commentRequestDTO.getNoticeId())
+                .build();
+    }
 }
