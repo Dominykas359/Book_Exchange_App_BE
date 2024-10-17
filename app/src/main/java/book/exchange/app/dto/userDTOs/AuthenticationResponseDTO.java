87fd4f0712e220ac1c1226c86b33ee83bbd5603a
Dominykas Stunžėnas <dominykas.stunzenas@stud.vilniustech.lt>
2024-10-17 10:32:28 +0300

    new DTO for authentication

--- app/src/main/java/book/exchange/app/dto/userDTOs/AuthenticationResponseDTO.java
+++ app/src/main/java/book/exchange/app/dto/userDTOs/AuthenticationResponseDTO.java
@@ -1,2 +1,16 @@
-package book.exchange.app.dto.userDTOs;public class AuthenticationResponseDTO {
+package book.exchange.app.dto.userDTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@SuperBuilder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationResponseDTO extends UserResponseDTO{
+
+    private String token;
 }
