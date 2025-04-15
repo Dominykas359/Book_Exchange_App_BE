9e97b2f32022861c1ae048bfa941c9d4341ae458
Dominykas Stunžėnas <dominykas.stunzenas@stud.vilniustech.lt>
2025-04-15 13:49:42 +0300

    vietoje konstruktoriaus naudotojama @AllArgsConstructor anotacija

--- app/src/main/java/book/exchange/app/model/WishlistItem.java
+++ app/src/main/java/book/exchange/app/model/WishlistItem.java
@@ -5,13 +5,11 @@ import lombok.*;
 import java.util.UUID;
 @Data
 @NoArgsConstructor
-//@AllArgsConstructor
+@AllArgsConstructor
 @Builder
 public class WishlistItem {
     UUID id;
     UUID user_id;
     UUID notice_id;
 
-    public WishlistItem(UUID uuid, UUID userId, UUID noticeId) {
-    }
 }
